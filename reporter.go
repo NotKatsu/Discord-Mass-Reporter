9066105a550121f1f5204674ca8b2dd8517048b7
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-04-16 15:56:45 +0200

    Check if string is a number

--- reporter.go
+++ reporter.go
@@ -8,9 +8,13 @@ import (
 var digitCheck = regexp.MustCompile(`^[0-9]+$`)
 
 func main() {
-	var user_id int
+	var user_id string
 
 	fmt.Printf("Discord ID: ")
 	fmt.Scanln(&user_id)
+
+	if digitCheck.MatchString(user_id) == true {
+		fmt.Println("Is Number.")
+	}
 
 }
