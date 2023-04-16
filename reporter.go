cb46510bd696a61fb761a9f5bc1142ed7e577b49
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-04-16 17:53:25 +0200

    user information fetching added

--- reporter.go
+++ reporter.go
@@ -2,10 +2,41 @@ package main
 
 import (
 	"fmt"
-	"regexp"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/NotKatsu/Discord-Mass-Reporter/helpers"
 )
 
-var digitCheck = regexp.MustCompile(`^[0-9]+$`)
+var base_url string = "https://discord.com/api/v9"
+
+func user(user_token string) {
+	req, err := http.NewRequest("GET", base_url+"/users/@me", nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	req.Header.Set("Authorization", "MTA1Mjk4MjcyMTU5ODczODUyMg.GuquGC.tqwUdU7Hyn4EFom2QH31_T2_ZAOu2m1m1q7QxI")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println(string(body))
+	}
+}
 
 func main() {
 	var user_id string
@@ -13,8 +44,7 @@ func main() {
 	fmt.Printf("Discord ID: ")
 	fmt.Scanln(&user_id)
 
-	if digitCheck.MatchString(user_id) == true {
-		fmt.Println("Is Number.")
+	if helpers.IntCheck(user_id) == true {
+		user()
 	}
-
 }
